656f317d77ec7b0700f6f19446c49a998b63ab28
Kairi <kairijb@gmail.com>
2018-11-08 16:29:33 -0600

    notes container is responsive and notes don't spill out the container

--- src/components/Notes/Note.js
+++ src/components/Notes/Note.js
@@ -6,12 +6,48 @@ const Note = () => {
         <h1>Your Notes:</h1>
         <div className='notes'>
 
+            <div className='note'>
+                <h4>Card title</h4>
+                <p>Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <button>Button</button>
+            </div>
 
             <div className='note'>
                 <h4>Card title</h4>
                 <p>Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                 <button>Button</button>
             </div>
+
+            <div className='note'>
+                <h4>Card title</h4>
+                <p>Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <button>Button</button>
+            </div>
+
+            <div className='note'>
+                <h4>Card title</h4>
+                <p>Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <button>Button</button>
+            </div>
+
+            <div className='note'>
+                <h4>Card title</h4>
+                <p>Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <button>Button</button>
+            </div>
+
+            <div className='note'>
+                <h4>Card title</h4>
+                <p>Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <button>Button</button>
+            </div>
+
+            <div className='note'>
+                <h4>Card title</h4>
+                <p>Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <button>Button</button>
+            </div>
+
         </div>
 
 
